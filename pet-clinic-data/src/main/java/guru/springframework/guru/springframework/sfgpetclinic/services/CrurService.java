28a535555673c736ca7d56ad5813808923e1a706
chenchuk <chenchuk@gmail.com>
2019-07-15 13:17:31 +0300

    chen: refactor interfaces to use common interface

---
+++ pet-clinic-data/src/main/java/guru/springframework/guru/springframework/sfgpetclinic/services/CrurService.java
@@ -0,0 +1,13 @@
+package guru.springframework.guru.springframework.sfgpetclinic.services;
+
+import java.util.Set;
+
+public interface CrurService<T, ID> {
+    public Set<T> findAll();
+
+    T findById(ID id);
+    T save(T object);
+    void delete(T object);
+    void deleteById(ID id);
+
+}
--- pet-clinic-data/src/main/java/guru/springframework/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -3,10 +3,7 @@ package guru.springframework.guru.springframework.sfgpetclinic.services;
 import guru.springframework.sfgpetclinic.model.Owner;
 import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrurService<Owner, Long>{
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
